79a043b1929a8cbc493907fea713730f2ed4ece2
Andrea Piza <carmenandreapiza@gmail.com>
2022-11-07 19:05:31 -0500

    Initial commit

---
+++ app/src/main/java/com/example/apppaseadores/RecuperarContraseña.kt
@@ -0,0 +1,11 @@
+package com.example.apppaseadores
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecuperarContraseña : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
